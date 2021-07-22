4ce2e899df207a42f9c515d009d53e6f5c0ad595
jhonnjc15 <jhonatanjcca@gmail.com>
2021-07-22 21:07:38 +0000

    Functions that goes through a binary tree using pre, in and post-order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - Function that goes through a binary 
+ * binary_tree_postorder - Function that goes through a binary
  * tree using post-order traversal
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
